3bb00a4eddcff6c2d82478e9380b390478cfc4db
Cédric Donner <cedonner@gmail.com>
2015-01-13 17:09:52 +0000

    ajout algos de recherche binaire

---
+++ chap8-structures/search/binary_search.py
@@ -0,0 +1,33 @@
+from random import randint
+
+nombres = [1, 4, 7, 3, 9, 24]
+
+# pour que l'algorithme fonctionne, il faut que la liste soit triée
+nombres.sort()
+
+def search(n, start=0, end=None):
+    
+    # pour donner à la variable locale 'end' une valeur par défaut de len(nombres) - 1
+    end = end or len(nombres) - 1
+    
+    middle = (start + end) // 2
+    if start == middle:
+        if n == nombres[start]:
+            return start
+        else:
+            return None
+            
+    if n < nombres[middle]:
+        return search(n, start=start, end=middle)
+        
+    elif n > nombres[middle]:
+        return search(n, start=middle, end=end)
+        
+    else:
+        # n se trouve forcément dans la liste
+        return middle
+        
+        
+for (i, n) in enumerate(nombres):
+    print(n, search(n), i)
+        
---
+++ chap8-structures/search/binary_search_iter.py
@@ -0,0 +1,29 @@
+from random import randint
+
+nombres = [1, 4, 7, 3, 9, 24]
+
+# pour que l'algorithme fonctionne, il faut que la liste soit triée
+nombres.sort()
+
+def search(liste, item):
+    
+    low = 0
+    high = len(liste) -1
+    
+    while low <= high:
+        middle = (low + high) // 2
+        guess = liste[middle]
+        
+        if guess == item:
+            return middle
+            
+        if guess >= item:
+            high = middle - 1
+        else:
+            low = middle + 1
+            
+    return None
+        
+def test():        
+    for (i, n) in enumerate(nombres + [30]):
+        print(search(nombres, n), i)
