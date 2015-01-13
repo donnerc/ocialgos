4a36045dc3f24a06624e2efc0936c4b49b001767
Cédric Donner <cedonner@gmail.com>
2015-01-13 17:16:08 +0000

    petite modif dans recherche binaire itérative : ajout référence

--- chap8-structures/search/binary_search_iter.py
+++ chap8-structures/search/binary_search_iter.py
@@ -1,3 +1,6 @@
+# Code de recherche binaire dans une liste triée issue du magnifique ouvrage
+# "Grokking algorihthms, http://www.manning.com/bhargava/
+
 nombres = [1, 4, 7, 3, 9, 24]
 
 # pour que l'algorithme fonctionne, il faut que la liste soit triée
